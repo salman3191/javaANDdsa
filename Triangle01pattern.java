c9963f71f544ba59f6a853828af70a7d62437507
salman <mohammadsalmankhan3191@gmail.com>
2025-02-11 12:13:49 +0530

    this is 01 triangle pattern code

---
+++ Triangle01pattern.java
@@ -0,0 +1,18 @@
+public class Triangle01pattern {
+    public static void trianglepattern(int rows){
+for(int i=1;i<=rows;i++){
+    for(int j=1;j<=i;j++){
+        if((i+j)%2==0){
+            System.out.print(1);
+        }else{
+            System.out.print(0);
+        }
+       
+    }
+    System.out.println();
+}
+    }
+    public static void main(String[] args) {
+        trianglepattern(4);
+    }
+}
