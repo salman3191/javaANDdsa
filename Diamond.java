e6a61088b6fecf2b21ff6962712a0d8714d5e368
salman <mohammadsalmankhan3191@gmail.com>
2025-02-11 22:28:39 +0530

    this code is for Diamond pattern

---
+++ Diamond.java
@@ -0,0 +1,25 @@
+public class Diamond{
+    public static void Diamondpattern(int rows){
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=rows;i>=1;i--){
+            for(int j=1;j<=rows-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Diamondpattern(4);
+    }
+}
