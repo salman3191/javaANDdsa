8e9dee48cbc35b6d867a3299d0ee27a0634e00de
salman <mohammadsalmankhan3191@gmail.com>
2025-02-16 16:36:11 +0530

    this is how we use prefix sum to find maximum of subArrays amd here now time Complexity is O(n^2)

--- subarrays.java
+++ subarrays.java
@@ -3,27 +3,29 @@ package javaDsa;
 public class subarrays {
     public static void SubArrays(int numbers[]){
         int largest=Integer.MIN_VALUE;
+        int prefix[]=new int[numbers.length];
+        int sum=0;
+        prefix[0]=numbers[0];
+        for(int i=1;i<numbers.length;i++){
+            prefix[i]=prefix[i-1]+numbers[i];
+        }
         for(int i=0;i<numbers.length;i++){
             int start=i;
             for(int j=i;j<numbers.length;j++){
                 int end=j;
-                int sum=0;
-                for(int k=start;k<=end;k++ ){ 
-                    System.out.print(numbers[k]+" ");
-                    sum=sum+numbers[k];
-                }
+                 sum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+               
                 if(largest<sum){
                     largest=sum;
                 }
-                System.out.print(" sum is : "+sum);
-                System.out.println(); 
+                
             }
-            System.out.println();
+            
         }
-         System.out.println("largest of subaarys is"+largest);
+         System.out.println("max of subaarys is"+" "+largest);
     }
     public static void main(String[] args) {
-        int numbers[]={2,4,6,8,10};
+        int numbers[]={1,-2,6,-1,3};
         SubArrays(numbers);
     }
 }
