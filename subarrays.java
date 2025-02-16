99b1c5cfc1ba1b1468390308925de5455eac801d
salman <mohammadsalmankhan3191@gmail.com>
2025-02-16 12:34:27 +0530

    this is how i solve my assignment question i.e print sum of subarrays and find maximun sum

--- subarrays.java
+++ subarrays.java
@@ -2,25 +2,29 @@ package javaDsa;
 
 public class subarrays {
     public static void SubArrays(int numbers[]){
-       
+        int largest=Integer.MIN_VALUE;
         for(int i=0;i<numbers.length;i++){
             int start=i;
             for(int j=i;j<numbers.length;j++){
                 int end=j;
-            
-                for(int k=start;k<end;k++ ){
+                int sum=0;
+                for(int k=start;k<=end;k++ ){ 
                     System.out.print(numbers[k]+" ");
-         
+                    sum=sum+numbers[k];
                 }
-                System.out.println();
-      
-     }
-       System.out.println();
+                if(largest<sum){
+                    largest=sum;
+                }
+                System.out.print(" sum is : "+sum);
+                System.out.println(); 
+            }
+            System.out.println();
         }
-    
+         System.out.println("largest of subaarys is"+largest);
     }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10};
         SubArrays(numbers);
     }
 }
+
