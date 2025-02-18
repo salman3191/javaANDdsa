4ea9fb798017a7d240c8dba5891a0bdc9e2c7f4a
salman <mohammadsalmankhan3191@gmail.com>
2025-02-18 17:43:40 +0530

    buy and sell stocks code

---
+++ stocks.java
@@ -0,0 +1,21 @@
+package javaDsa;
+
+public class stocks {
+    public static int buyAndSellstocks(int prices[]){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxprofit=0;
+        for(int i=0;i<prices.length;i++){
+            if(buyPrice<prices[i]){
+                int profit=prices[i]-buyPrice;
+                maxprofit=Math.max(maxprofit, profit);
+            }else{
+                buyPrice=prices[i];
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(buyAndSellstocks(prices));
+    }
+}
