884d2a8fa42b28e2248ce8bf3b22a9cec40477a8
salman <mohammadsalmankhan3191@gmail.com>
2025-02-20 19:17:49 +0530

    code for selection sort array

--- bubblesort.java
+++ bubblesort.java
@@ -23,9 +23,23 @@ public class bubblesort {
             System.out.print(arr[i]+" ");
         }
     }
+    public static void selectionsort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minpos=i;
+            for(int j=i+1;j<arr.length;j++){ //for descending order use <
+                if(arr[minpos]>arr[j]){
+                    minpos=j;
+                }
+            }
+            // swap
+            int temp=arr[minpos];
+            arr[minpos]=arr[i];
+            arr[i]=temp;
+        }
+    }
     public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
-        BubbleSort(arr);
+        selectionsort(arr);
         printArr(arr);
     }
 }
