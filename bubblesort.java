763ceaaebd7e7bb1c1fa5f1a19529b7122f41e22
salman <mohammadsalmankhan3191@gmail.com>
2025-02-20 12:50:44 +0530

    Bubble sort algorithm

---
+++ bubblesort.java
@@ -0,0 +1,31 @@
+package javaDsa;
+
+public class bubblesort {
+    public static void BubbleSort(int arr[]){
+        int n=arr.length;
+        for(int turn=0;turn<n;turn++){
+            for(int j=0;j<n-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    // swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    
+        
+                }
+            }
+        }
+
+      
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        BubbleSort(arr);
+        printArr(arr);
+    }
+}
