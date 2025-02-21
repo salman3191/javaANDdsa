f4e899af443cad2df90ce5a1fc4405eeb1ff601b
salman <mohammadsalmankhan3191@gmail.com>
2025-02-21 17:45:05 +0530

    code for insertion Sort

--- bubblesort.java
+++ bubblesort.java
@@ -36,10 +36,25 @@ public class bubblesort {
             arr[minpos]=arr[i];
             arr[i]=temp;
         }
+
+    }
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            // finding correct pos to insert
+            while(prev >=0   &&   arr[prev] > curr){
+            arr[prev+1]=arr[prev];
+            prev--;
+            }
+            // insertion
+            arr[prev+1]=curr;
+        }
+        
     }
     public static void main(String[] args) {
         int arr[]={5,4,1,3,2};
-        selectionsort(arr);
+        insertionSort(arr);
         printArr(arr);
     }
 }
