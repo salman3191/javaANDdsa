dadc52309c37fea884c77a501541f665d501eaeb
salman <mohammadsalmankhan3191@gmail.com>
2025-04-12 22:16:25 +0530

    this code is how we check whether the string is palindrom or not

---
+++ strings.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class strings {
+    public static boolean palindrome(String str){
+   for(int i=0;i<str.length()/2;i++){
+    int n=str.length();
+   if(str.charAt(i)!=str.charAt(n-i-1)){
+ return false;
+   }
+   }
+   return true;
+    }
+    public static void main(String[] args) {
+        String str="madam";
+          System.out.println(palindrome(str));
+    }
+}
