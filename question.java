41f75d5d8794316e66b0cf41820c194bb217def6
salman <mohammadsalmankhan3191@gmail.com>
2025-04-13 00:21:48 +0530

    this code is for shortespath question

--- question.java
+++ question.java
@@ -1,6 +1,6 @@
 package Strings;
 public class question {
-    public static int shortestpath(String str){
+    public static double shortestpath(String str){
         int x=0,y=0;
         for(int i=0;i<str.length();i++){
             // east
@@ -24,5 +24,9 @@ public class question {
         int X2= x*x;
         int Y2=y*y;
         return (float)Math.sqrt(X2+Y2);
+    }
+    public static void main(String[] args) {
+        String str="WNEENESENNN";
+        System.out.println(shortestpath(str));
     }
 }
